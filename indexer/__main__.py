7253ed55dd15f0582683d3dff1eb9628e094fc60
Thomas Marchand <thomas.marchand@tuta.io>
2023-07-27 19:14:52 +0200

    fix: not restarting correclty

--- indexer/__main__.py
+++ indexer/__main__.py
@@ -9,14 +9,15 @@ from aiohttp import web
 from pymongo import MongoClient
 import aiohttp_cors
 
+
 async def start_server(conf, events_manager):
     app = web.Application()
-    app.add_routes([web.get('/is_ready', is_ready)])
-    app.add_routes([web.get('/get_renewal_data', get_renewal_data)])
-    app['endpoint'] = Endpoints(events_manager)
+    app.add_routes([web.get("/is_ready", is_ready)])
+    app.add_routes([web.get("/get_renewal_data", get_renewal_data)])
+    app["endpoint"] = Endpoints(events_manager)
     client = MongoClient(conf.connection_string)
     db = client[conf.indexer_id]
-    app['collection'] = db['auto_renewals']
+    app["collection"] = db["auto_renewals"]
     cors = aiohttp_cors.setup(
         app,
         defaults={
@@ -33,15 +34,14 @@ async def start_server(conf, events_manager):
     await runner.setup()
     site = web.TCPSite(runner, port=conf.server_port)
     await site.start()
-    stop_event = asyncio.Event()
-    await stop_event.wait()
     return runner
+
 
 async def main():
     conf = TomlConfig("config.toml", "config.template.toml")
     events_manager = Listener(conf)
     enable_ssl = False if conf.is_devnet is True else True
-    runner = IndexerRunner(
+    indexer_runner = IndexerRunner(
         config=IndexerRunnerConfiguration(
             stream_url=conf.apibara_stream,
             storage_url=conf.connection_string,
@@ -51,22 +51,23 @@ async def main():
         reset_state=conf.reset_state,
     )
 
-    runner_task = asyncio.create_task(runner.run(events_manager, ctx={"network": "starknet-mainnet"}))
     server_runner = await start_server(conf, events_manager)
-    web_server_task = asyncio.create_task(server_runner.start())
+    indexer_task = asyncio.create_task(
+        indexer_runner.run(events_manager, ctx={"network": "starknet-mainnet"})
+    )
 
-    await asyncio.gather(runner_task, web_server_task)
+    try:
+        await indexer_task
+    except Exception:
+        traceback.print_exc()
+        print("warning: exception detected, restarting")
+    finally:
+        await server_runner.cleanup()
     print("starknetid indexer started")
-    print('web_server started')
+    print("web_server started")
     return server_runner
 
 
 if __name__ == "__main__":
     while True:
-        try:
-            server_runner = asyncio.run(main())
-        except Exception:
-            print(traceback.format_exc())
-            print("warning: exception detected, restarting")
-            if server_runner is not None:
-                asyncio.run(server_runner.cleanup())
+        asyncio.run(main())
